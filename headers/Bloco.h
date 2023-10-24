de147c61a6180051aa42452c861b54a62cf2406c
gsherterton <gsherterton@gmail.com>
2023-10-24 15:47:49 -0300

    Alterando algumas coisas basicas relacionadas ao movimento e redimensionando as imagens

---
+++ headers/Bloco.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Bloco{
+    public:
+        int tamanho;
+        int posicaoX;
+        int posicaoY;
+
+        Bloco();
+        Bloco(int tamanho, int posicaoX, int posicaoY);
+};
---
+++ sources/Bloco.cpp
@@ -0,0 +1,9 @@
+#include "Bloco.h"
+
+Bloco::Bloco(){}
+
+Bloco::Bloco(int tamanho, int posicaoX, int posicaoY){
+    this->tamanho = tamanho;
+    this->posicaoX = posicaoX;
+    this->posicaoY = posicaoY;
+}
